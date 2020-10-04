7cda4fb8013a7978d655ce55fd23ab43edb33984
Julyme3 <jg0311@bk.ru>
2020-10-04 18:54:28 +0300

    настройка маршрутизации

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
-import MainPage from "../main-page/main-page";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import MainScreen from "../main-screen/main-screen";
+import AuthScreen from "../auth-screen/auth-screen";
+import RoomScreen from "../room-screen/room-screen";
+import FavoritesScreen from "../favorites-screen/favorites-screen";
 
 const App = (props) => {
 
   const {placesCount} = props;
 
   return (
-    <MainPage
-      placesCount={placesCount}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MainScreen placesCount={placesCount} />
+        </Route>
+        <Route path="/login" exact>
+          <AuthScreen />
+        </Route>
+        <Route path="/favorites" exact>
+          <FavoritesScreen />
+        </Route>
+        <Route path="/offer/:id" exact>
+          <RoomScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
